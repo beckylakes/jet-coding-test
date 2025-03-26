49a806b0961b267eca6ce748c13512b3211a1e2f
beckylakes <beckylake96@gmail.com>
2025-03-26 12:58:20 +0000

    Added search bar, now implement functionality

---
+++ components/SearchBar.jsx
@@ -0,0 +1,12 @@
+const SearchBar = () => {
+
+  return (
+    <input
+      type="text"
+      placeholder="Enter postcode"
+      className="flex h-10 w-full rounded-2xl bg-gray-600 px-3 py-2"
+    />
+  );
+};
+
+export default SearchBar;
