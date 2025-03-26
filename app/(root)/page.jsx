d0c34cfa7a9a0b95af3d2eaeda276d99810d1cae
beckylakes <beckylake96@gmail.com>
2025-03-26 14:21:39 +0000

    successfully fetch data according to postcode entered

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -2,8 +2,9 @@ import Collection from "@/components/Collection";
 import SearchBar from "@/components/SearchBar";
 import { getRestaurants } from "@/utils/api";
 
-export default async function Home() {
-  const postcode = "KT25SY"
+export default async function Home({searchParams}) {
+  const finalSearchParams = await searchParams
+  const postcode = finalSearchParams.postcode
   const restaurants = await getRestaurants(postcode)
 
   return (
